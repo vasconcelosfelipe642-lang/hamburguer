22ec116b51408cff6370684f7d82dcde2d782639
vasconcelosfelipe642-lang <vasconcelosfelipe642@gmail.com>
2026-04-14 11:52:39 -0300

    atualização das rotas

--- routes/Entregaroute.js
+++ routes/Entregaroute.js
@@ -3,9 +3,9 @@ import EntregaController from '../controllers/EntregaController.js';
 
 const router = Router();
 
-router.post('/', EntregaController.store); 
 router.get('/', EntregaController.index);
 router.get('/:id', EntregaController.show);
+router.post('/', EntregaController.store);
 router.put('/:id', EntregaController.update);
 router.delete('/:id', EntregaController.destroy);
 
