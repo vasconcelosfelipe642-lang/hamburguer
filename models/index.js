1533183d88b08e4d47e9ccf41a70ff793455bffb
vasconcelosfelipe642-lang <vasconcelosfelipe642@gmail.com>
2026-04-17 00:21:21 -0300

    array para json(chaves)

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ import Pedido from './pedido.js';
 import Entrega from './entrega.js';
 import Avaliacao from './avaliacao.js';
 
-const models = [Categoria, Produto, Pedido, Entrega, Avaliacao];
+const models = {Categoria, Produto, Pedido, Entrega, Avaliacao};
 
 Object.values(models).forEach(model => {
   if (model.associate) {
